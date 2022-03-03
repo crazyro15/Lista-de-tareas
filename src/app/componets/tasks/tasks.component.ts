b6144016d4b43a6b67df119ab662cc89a3cddee4
crazyro15 <crazyro15@hotmail.com>
2022-03-02 22:28:04 -0300

    finalizado

--- src/app/componets/tasks/tasks.component.ts
+++ src/app/componets/tasks/tasks.component.ts
@@ -11,14 +11,35 @@ export class TasksComponent implements OnInit {
   tasks: Task[] = [];
 
   constructor(
-    private taskservice:TaskService
+    private taskService:TaskService
   ) { }
 
   ngOnInit(): void {
     //Like Promise
-    this.taskservice.getTasks().subscribe(tasks=>[
+    this.taskService.getTasks().subscribe((tasks)=>{
       this.tasks = tasks
-    ])
+    });
+  }
+
+  deleteTask(task:Task){
+    this.taskService.deleteTask(task)
+    .subscribe(
+      ()=>{
+      this.tasks = this.tasks.filter( (t) =>{ 
+      return t.id !== task.id
+      })
+    })
+  }
+
+  toggleReminder(task:Task){
+    task.reminder = !task.reminder
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
+
+  addTask(task:Task){
+    this.taskService.addTask(task).subscribe((task) =>{
+      this.tasks.push(task)
+    })
   }
 
 }
